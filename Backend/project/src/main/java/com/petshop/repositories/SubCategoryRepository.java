17e3c4633090f64aede77f7ac391f8bea2cac8e7
haquocviet261 <haquocviet261@gmail.com>
2023-12-14 14:41:57 +0700

    update all

--- Backend/project/src/main/java/com/petshop/repositories/SubCategoryRepository.java
+++ Backend/project/src/main/java/com/petshop/repositories/SubCategoryRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface SubCategoryRepository extends JpaRepository<SubCategory,Long> {
-    @Query("select s from SubCategory s  where s.category = :category_id ")
+    @Query("select s from SubCategory s  where s.category.category_id = :category_id ")
     public List<SubCategory> findSubcategoryByCategoryId(@Param("category_id") Long category_id );
     public List<SubCategory> findAll();
 }
