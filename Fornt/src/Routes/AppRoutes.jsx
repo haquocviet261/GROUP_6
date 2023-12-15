f2655d3b2f7b8c72927461a97bb790db25a65201
linhtmhe163233 <linhtmhe163233@fpt.edu.vn>
2023-12-15 10:03:51 +0700

    test

--- Fornt/src/Routes/AppRoutes.jsx
+++ Fornt/src/Routes/AppRoutes.jsx
@@ -1,9 +1,13 @@
 import { Route, Routes, Link } from "react-router-dom";
 import LoginPage from "../Components/LoginPage";
-import HomePage from "../Components/HomePage";
+//import HomePage from "../Components/HomePage";
 import SearchBar from "../Components/SearchBar";
 import PrivateRoutes from "./PrivateRoutes";
 import AdminPage from "../Components/AdminPage";
+import HomePage from "../Components/HomePage";
+import RegisterAccount from "../Components/RegisterAccount";
+import ForgotPassword from "../Components/ForgotPasswordPage";
+
 
 const AppRoutes = () => {
   // if(localStorage.getItem("token") == null){
@@ -14,22 +18,22 @@ const AppRoutes = () => {
 
   return (
     <Routes>
-    <>
-     
+      <>
+
         <Route path="/" element={<HomePage />} />
         <Route path="test" element={<SearchBar />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterAccount/>}></Route>
-        <Route path="/forgotpassword" element={<ForgotPassword/>}/>
-        <Route path="/newpassword" element={<NewPassword/>}/>
-     
-      
-{/*       
+        <Route path="/register" element={<RegisterAccount />}></Route>
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/newpassword" element={<NewPassword />} />
+
+
+        {/*       
       <PrivateRoutes path="/Admin">
         <AdminPage />
       </PrivateRoutes> */}
-     
-    </>
+
+      </>
     </Routes>
   );
 };
