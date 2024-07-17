37c74add8cbde1fb47e740248cb07ce8548888fa
haquocviet261 <haquocviet261@gmail.com>
2024-07-17 17:31:29 +0700

    update client inbound

--- Backend/project/src/main/java/com/petshop/repositories/FoodItemRepository.java
+++ Backend/project/src/main/java/com/petshop/repositories/FoodItemRepository.java
@@ -9,7 +9,7 @@ import java.sql.Date;
 import java.util.List;
 
 public interface FoodItemRepository extends JpaRepository<FoodItem,Long> {
-    @Query("select f from FoodItem f where f.deviceItem.device_item_id = : device_item_id")
+    @Query("select f from FoodItem f where f.deviceItem.device_item_id = :device_item_id")
     List<FoodItem> getFoodItemByDeviceItemID(@Param("device_item_id") Long device_item_id);
     @Query("select f from FoodItem f where f.expiration_date <= DATEADD(day, -1, :currentDate) ")
     List<FoodItem> getFoodItemsByDeviceItemID(@Param("currentDate") Date currentDate);
