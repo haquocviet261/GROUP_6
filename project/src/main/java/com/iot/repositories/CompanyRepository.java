72093caa607eb041fc8d16301b822b381d00b9c2
ductaviet11 <tavietduc.da@gmail.com>
2024-10-24 18:27:03 +0700

    zzz

---
+++ project/src/main/java/com/iot/repositories/CompanyRepository.java
@@ -0,0 +1,12 @@
+package com.iot.repositories;
+
+import com.iot.model.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CompanyRepository extends JpaRepository<Company,Long> {
+    @Query("SELECT DISTINCT c.name FROM Company c")
+    List<String> findDistinctCompanyNames();
+}
---
+++ project/src/main/java/com/iot/services/imp/CompanyServiceImp.java
@@ -0,0 +1,20 @@
+package com.iot.services.imp;
+
+import com.iot.common.utils.Validation;
+import com.iot.model.dto.response.ResponseObject;
+import com.iot.repositories.CompanyRepository;
+import com.iot.services.interfaces.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CompanyServiceImp implements CompanyService {
+    @Autowired
+    CompanyRepository companyRepository;
+
+    @Override
+    public ResponseEntity<ResponseObject> getAllNameCompany() {
+        return ResponseEntity.ok(new ResponseObject(Validation.OK,"Successfully !!!", companyRepository.findDistinctCompanyNames()));
+    }
+}
